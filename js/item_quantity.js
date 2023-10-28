d50c2e1697178712a06457db076f788fe73c8459
Exequiel <exemacchi27@gmail.com>
2023-10-28 00:18:32 -0300

    v0.10.0 Item_html: styles and functionalities completed
    FEAT: se mejora la clase `.main-container` para que sea reutilizable en todas las páginas.
    FEAT: se agrega la página `item.html` con todos sus estilos y funcionalidades.

---
+++ js/item_quantity.js
@@ -0,0 +1,16 @@
+// Aumentar o disminuir la cantidad de un input
+window.onload = function() {
+    const input = document.querySelector("#item-quantity");
+    const plusButton = document.querySelector("#plus-button");
+    const minusButton = document.querySelector("#minus-button");
+
+    // Cuando se haga click en el botón +
+    plusButton.addEventListener("click", () => input.value++);
+
+    // Cuando se haga click en el botón -
+    minusButton.addEventListener("click", () => {
+        if (input.value > 0) {
+            input.value -= 1;
+        }
+    })
+}
